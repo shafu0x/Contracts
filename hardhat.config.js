10572c6dbfa411687967ea59781c6d13abee654b
Brian Soule <brian@briansoule.com>
2021-08-16 21:50:46 -0500

    Added goerli network to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+    networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+  },
 };
