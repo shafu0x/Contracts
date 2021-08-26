d17e3803b2cf4f8606ebd487365cc7383d3fecbd
Brian Soule <brian@briansoule.com>
2021-08-26 00:50:51 -0500

    Made subdomain configurator ownable

--- contracts/SubdomainConfig.sol
+++ contracts/SubdomainConfig.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Interfaces.sol";
 
 // Operating the subdomain configurator:
@@ -10,7 +11,7 @@ import "./Interfaces.sol";
 
 // Optional: Specify an owner that is the only address allowed to query this contract
 
-contract SubdomainConfig {
+contract SubdomainConfig is Ownable {
     
     ENSRegistryWithFallback public ensRegistry;
     PublicResolver public publicResolversResolver;
@@ -18,19 +19,21 @@ contract SubdomainConfig {
 
     bytes32 constant TKR_NAMEHASHED_NODE = 0x3fb945f27ea7fe9357d05b97b921898a90a2cbf8fc7e092e94bcacec9a80da0c;
     bytes32 constant RESOLVER_DOT_ETH_NAMEHASHED_NODE = 0xfdd5d5de6dd63db72bbc2d487944ba13bf775b50a80805fe6fcaba9b0fba88f5;
+    
 
     constructor() {
         ensRegistry = ENSRegistryWithFallback(0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e);
         
         publicResolversResolver = PublicResolver(ensRegistry.resolver(RESOLVER_DOT_ETH_NAMEHASHED_NODE));
         publicResolver = PublicResolver(publicResolversResolver.addr(RESOLVER_DOT_ETH_NAMEHASHED_NODE));
+        
     }
     
     // tickerLabel: the keccak256 of the ticker string only
     // owner: the address that will ultimately control the subdomain, most likely the multisig
     // finalNode: the namehash of the full domain; ie ticker.tkn.eth
     // mainnetTokenAddress: the address where the token contract for the ticker is located
-    function configureSubdomain(bytes32 tickerLabel, address owner, bytes32 fullNameNode, address mainnetTokenAddress) public {
+    function configureSubdomain(bytes32 tickerLabel, address owner, bytes32 fullNameNode, address mainnetTokenAddress) onlyOwner public {
         ensRegistry.setSubnodeRecord(TKR_NAMEHASHED_NODE, tickerLabel, address(this), address(publicResolver), 0);
         publicResolver.setAddr(fullNameNode, mainnetTokenAddress);
         ensRegistry.setSubnodeOwner(TKR_NAMEHASHED_NODE, tickerLabel, owner);
