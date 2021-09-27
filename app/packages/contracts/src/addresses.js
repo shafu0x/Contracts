c537b0e35ddae0f2d207c798f720d88740711c21
Brian Soule <brian@briansoule.com>
2021-09-27 01:10:43 -0500

    Point to TKR

--- app/packages/contracts/src/addresses.js
+++ app/packages/contracts/src/addresses.js
@@ -1,7 +1,7 @@
 // This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
-  ceaErc20: "0xc1C0472c0C80bCcDC7F5D01A376Bd97a734B8815",
+  ceaErc20: "0xF7494A3298b1a6C2AFe60B3A3FD6389D7f400eA5",
 };
 
 export default addresses;
