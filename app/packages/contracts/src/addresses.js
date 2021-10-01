dbd5d2bd813e6f50c2dbef3392b648bd51bfeb4e
Brian Soule <brian@briansoule.com>
2021-10-01 07:58:23 -0500

    Pointer to deployed merkle distributor contract

--- app/packages/contracts/src/addresses.js
+++ app/packages/contracts/src/addresses.js
@@ -2,6 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xF7494A3298b1a6C2AFe60B3A3FD6389D7f400eA5",
+  merkleDistributor: "0x9238f67ad112aF2466fD7304A9F3d7d7dD88071d"
 };
 
 export default addresses;
