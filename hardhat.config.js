55671dcffb4b25c498ba0bc8ed228de0304dd914
Brian Soule <brian@briansoule.com>
2021-10-01 08:01:01 -0500

    Updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,23 +18,38 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-    networks: {
-      mainnet: {
-        // url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
-        url: `http://localhost:8545`,
-        accounts: [`0x${MAIN_HOT_KEY}`],
+  // paths: {
+  //   sources: "./contracts/TKR/TickerDao.sol",
+  // },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
       },
-      goerli: {
-        url: `https://eth-goerli.alchemyapi.io/v2/${GOERLI_ALCHEMY_API_KEY}`,
-        accounts: [`0x${GOERLI_PRIVATE_KEY}`],
-      },
-      goerliinfura: {
-        url: 'https://goerli.infura.io/v3/a31dc24ca4e843e98597e3048dae047c',
-        accounts: [`0x${GOERLI_PRIVATE_KEY}`],
-      },
+      {
+        version: "0.6.11",
+      }
+    ]
   },
-    abiExporter: {
+  networks: {
+    mainnet: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/TOsBQrnH33jGzGZPMwQ5u3IpbBydlwaL",
+      accounts: [`0x${MAIN_HOT_KEY}`],
+    },
+    local: {
+      url: `http://localhost:8545`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${GOERLI_ALCHEMY_API_KEY}`,
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+    goerliinfura: {
+      url: 'https://goerli.infura.io/v3/a31dc24ca4e843e98597e3048dae047c',
+      accounts: [`0x${GOERLI_PRIVATE_KEY}`],
+    },
+  },
+  abiExporter: {
     path: './contracts/abi',
     clear: true,
     flat: true,
