976a71f18a74a504f46bf9b9be1effe122f58b59
Brian Soule <brian@briansoule.com>
2021-10-01 08:04:44 -0500

    Updated script to create merkle airdrop lists

--- scripts/create-merkleable-file.js
+++ scripts/create-merkleable-file.js
@@ -1,16 +1,36 @@
 var json = require('../data/airdrop/ens_token_holders.json'); //(with path)
 
-var processedAddresses = {};
+// var processedAddresses = new Array()
+var processedAddresses = new Object()
 
 for (var prop in json) {
-  console.log(prop)
-  processedAddresses[json[prop]["address"]] = 100;
+  processedAddresses[json[prop].address] = 100000000000000000000
+
+  // console.log(prop)
+  // let claim = new Object
+  // claim[json[prop].address] = 100
+  // claim.address = json[prop].address
+  // claim.amount = 100
+
+  // processedAddresses.push(claim)
+
+  // processedAddresses[json[prop]["address"]] = 100;
+  
 }
-console.log(processedAddresses)
+// console.log(processedAddresses)
+
+let output = JSON.stringify(processedAddresses, null, 4)
 
 var fs = require('fs');
-fs.writeFile (__dirname + "/../data/airdrop/recipients.json", JSON.stringify(processedAddresses), function(err) {
+fs.writeFile (__dirname + "/../data/airdrop/recipients.json", output, function(err) {
   if (err) throw err;
   console.log('complete');
   }
 );
+
+
+// fs.writeFile (__dirname + "/../data/airdrop/recipients.json", "sfasdfasdfs", function(err) {
+//   if (err) throw err;
+//   console.log('complete');
+//   }
+// );
