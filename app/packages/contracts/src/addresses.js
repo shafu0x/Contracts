d30086f3ab48ef12b0c88b3eff5b89041e430956
Brian Soule <brian@briansoule.com>
2021-10-01 08:03:05 -0500

    Added address of merkle distributor

--- app/packages/contracts/src/addresses.js
+++ app/packages/contracts/src/addresses.js
@@ -2,7 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xF7494A3298b1a6C2AFe60B3A3FD6389D7f400eA5",
-  merkleDistributor: "0x9238f67ad112aF2466fD7304A9F3d7d7dD88071d"
+  merkleDistributor: "0x995cc213E943B7Fa84Fe15B63a3fb21eC11cFcE6"
 };
 
 export default addresses;
