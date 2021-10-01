79be8c576518a8bff74fcdb331ab52d3e34dd2de
Brian Soule <brian@briansoule.com>
2021-10-01 08:07:30 -0500

    Updated merkledistributor address

--- app/packages/contracts/src/addresses.js
+++ app/packages/contracts/src/addresses.js
@@ -2,6 +2,7 @@
 // Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
 const addresses = {
   ceaErc20: "0xF7494A3298b1a6C2AFe60B3A3FD6389D7f400eA5",
+  merkleDistributor: "0x995cc213E943B7Fa84Fe15B63a3fb21eC11cFcE6"
 };
 
 export default addresses;
