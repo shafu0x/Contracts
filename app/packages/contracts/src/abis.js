55372ded4e760924cc50860e08489fab4f020932
Brian Soule <brian@briansoule.com>
2021-10-01 08:08:58 -0500

    Added merkle distributor abi

--- app/packages/contracts/src/abis.js
+++ app/packages/contracts/src/abis.js
@@ -1,9 +1,11 @@
 import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
+import merkleDistributorAbi from "./abis/merkleDistributor.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
+  merkleDistributor: merkleDistributorAbi
 };
 
 export default abis;
