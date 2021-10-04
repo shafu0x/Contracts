81008fdaf7c03068c35739d7c58a81c24620c418
Brian Soule <brian@briansoule.com>
2021-10-04 02:00:57 -0500

    Name purchaser script

---
+++ scripts/name-purchaser.js
@@ -0,0 +1,72 @@
+const namehash = require('eth-ens-namehash')
+const { ethers } = require("hardhat");
+
+const ensAbi = require('../resources/ens_abi.json')
+const publicResolverAbi = require('../resources/public_resolver_abi.json')
+const tokenData = require('../data/seed_erc20s.json')
+
+const ensAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+
+let gasPrice = 20000000000
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  let resolverAddr = await ethers.provider.resolveName("resolver.eth")
+
+  console.log("Public resolver address:", resolverAddr)
+
+  const ensContract = new ethers.Contract(ensAddress, ensAbi, deployer)
+  const resolverContract = new ethers.Contract(resolverAddr, publicResolverAbi, deployer)
+
+  let pendingTokens = [];
+
+  for (let [key, token] of Object.entries(tokenData)) {
+    for (let [key, token_contract] of Object.entries(token.token_contracts)) {
+      if (token_contract.chain_id == 1) {
+        // console.log(key, token_contract.contract_address);
+        token.mainnet_contract_address = token_contract.contract_address
+        pendingTokens.push(token)
+      }
+    }
+  }
+
+  for (let [, token] of Object.entries(pendingTokens)) {
+    if (token.failed_qa == true) {
+      console.log("Skipping", token.name, "which failed QA")
+    } else {
+      let normalizedTicker = namehash.normalize(token.ticker)
+      let resolvedName = await ethers.provider.resolveName(`${normalizedTicker}.tkn.eth`)
+      console.log("resolvedName:", resolvedName, token.ticker)
+
+      if (!resolvedName) {
+        console.log(normalizedTicker, "not set. Registering and configuring addr.", )
+
+        let subdomainLabel = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(normalizedTicker))
+
+        let node = namehash.hash('tkn.eth') 
+
+        let newSubdomainTx = await ensContract.setSubnodeRecord(node, subdomainLabel, deployer.address, resolverAddr, 0, { gasPrice: gasPrice })
+        console.log("Waiting for", normalizedTicker, "subnodeRecord transaction to mine at https://goerli.etherscan.io/tx/" + newSubdomainTx.hash)
+        await ethers.provider.waitForTransaction(newSubdomainTx.hash, 1);
+
+        let fullNode = namehash.hash(`${normalizedTicker}.tkn.eth`)
+        let newSubdomainAddrTx = await resolverContract.setAddr(fullNode, token.mainnet_contract_address, { gasLimit: 200000, gasPrice: gasPrice })
+
+        console.log("Waiting for", normalizedTicker, "setAddr transaction to mine at https://goerli.etherscan.io/tx/" + newSubdomainAddrTx.hash)
+        await ethers.provider.waitForTransaction(newSubdomainAddrTx.hash, 1);
+      }
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
