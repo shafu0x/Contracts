bb7038d4f793ac8f417732fa141ecbdbb13c775d
Brian Soule <brian@briansoule.com>
2021-12-26 12:17:13 -0600

    Partially WIP migrator

--- scripts/subdomain-migrator.js
+++ scripts/subdomain-migrator.js
@@ -5,10 +5,10 @@ const ensAbi = require('../resources/ens_abi.json')
 const publicResolverAbi = require('../resources/public_resolver_abi.json')
 const erc20Abi = require('../resources/erc20_abi.json')
 
-const tokenData = require('../data/seed_erc20s.json')
-const expandedTokenData = require('../data/final-final-migration-data.json')
+// const tokenData = require('../data/seed_erc20s.json')
+const expandedTokenData = require('../data/subdomains/final-final-non-eth-migration-data.json')
 
-const subdomainConfigAbi = require('../resources/subdomain_config_abi.json')
+const subdomainConfigAbi = require('../resources/subdomain_expanded_config_with_name_abi.json')
 
 // Detailed instructions for running this script
 // 1. Compile contract in remix or wherever
@@ -26,12 +26,13 @@ const subdomainConfigAbi = require('../resources/subdomain_config_abi.json')
 // 9. Run the script with `npx hardhat run scripts/subdomain-migrator.js --network mainnet`
 // 10. Comment out the `waitForTransaction` await line if you want to populate the mempool and not wait for each transaction to finish
 
-const subdomainMigratorContractAddr = '0x31f0214C176640ba8320ED233AB7ED414633e091'
+// const subdomainMigratorContractAddr = '0x31f0214C176640ba8320ED233AB7ED414633e091'
+const subdomainMigratorContractAddr = '0xd160f2aa4e1be450448741eba5f5345567c19387'
 
 const ensAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
 const domainOwner = '0x3A7cbf0a90DC6755DdEE66886Dd26d4A6Ab64896'
 
-let gasPrice = 30000000000
+let gasPrice = 42128698580
 let priorityFee = 1500000000
 
 async function main() {
@@ -49,51 +50,67 @@ async function main() {
 
   let pendingTokens = [];
 
-  for (let [key, token] of Object.entries(tokenData)) {
-    for (let [key, token_contract] of Object.entries(token.token_contracts)) {
-      if (token_contract.chain_id == 1) {
+  
 
-        // Check to see if tokens are in seed token data before running (first migration)
-        for (let [key, expandedToken] of Object.entries(expandedTokenData)) {
-          if (expandedToken.contract == token_contract.contract_address) {
-            pendingTokens.push(expandedToken)
-          }
-        }
+  // for (let [key, token] of Object.entries(tokenData)) {
+  //   for (let [key, token_contract] of Object.entries(token.token_contracts)) {
+  //     if (token_contract.chain_id == 1) {
 
-        // console.log(key, token_contract.contract_address);
-        // token.mainnet_contract_address = token_contract.contract_address
-        // pendingTokens.push(token)
-      }
-    }
-  }
+  //       // Check to see if tokens are in seed token data before running (first migration)
+  //       for (let [key, expandedToken] of Object.entries(expandedTokenData)) {
+  //         if (expandedToken.contract == token_contract.contract_address) {
+  //           pendingTokens.push(expandedToken)
+  //         }
+  //       }
 
-  for (let [, token] of Object.entries(pendingTokens)) {
-    if (token.failed_qa == true) {
-      console.log("Skipping", token.verbose.data.name, "which failed QA")
-    } else {
-      let normalizedTicker = namehash.normalize(token.verbose.data.symbol)
-      let resolvedName = await ethers.provider.resolveName(`${normalizedTicker}.tkn.eth`)
-      console.log("resolvedName:", resolvedName, token.verbose.data.symbol)
+  //       // console.log(key, token_contract.contract_address);
+  //       // token.mainnet_contract_address = token_contract.contract_address
+  //       // pendingTokens.push(token)
+  //     }
+  //   }
+  // }
 
-      if (!resolvedName) {
+  for (let [, token] of Object.entries(expandedTokenData)) {
+    // if (token.failed_qa == true) {
+    //   console.log("Skipping", token.verbose.data.name, "which failed QA")
+    // } else {
+      let normalizedTicker = namehash.normalize(token.data.symbol)
+      let fullDomain = `${normalizedTicker}.tkn.eth`;
+      // let resolvedName = await ethers.provider.resolveName(fullDomain);
+      // console.log("resolvedName:", resolvedName, token.data.symbol)
+
+      let fullNode = namehash.hash(fullDomain)
+      let currentOwner = await ensContract.owner(fullNode)
+      console.log("Domain owned by", domainOwner, "Symbol:", token.data.symbol, "Full domain:", fullDomain)
+
+      if (currentOwner == "0x0000000000000000000000000000000000000000") {
         console.log(normalizedTicker, "not set. Registering and configuring addr.", )
 
         // Encode your domain strings for the blockchain
         let subdomainLabel = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(normalizedTicker))
         let node = namehash.hash('tkn.eth')
-        let fullNode = namehash.hash(`${normalizedTicker}.tkn.eth`)
 
-        let tokenContract = new ethers.Contract(token.contract, erc20Abi, deployer)
-        let contractDecimals = await tokenContract.decimals();
+        // Commented out for migrating non etherem based tokens
+        // let tokenContract = new ethers.Contract(token.contract, erc20Abi, deployer)
+        // let contractDecimals = await tokenContract.decimals();
 
-        console.log("Decimals for", normalizedTicker, ":", contractDecimals)
+        // console.log("Decimals for", normalizedTicker, ":", contractDecimals)
 
-        console.log(subdomainLabel, domainOwner, fullNode, token.contract)
-        let newSubdomainAddrTx = await subdomainConfigContract.configureSubdomainFully(subdomainLabel, domainOwner, fullNode, token.contract, [token.url, token.avatar, `{\"rev\":0, \"decimals\":${contractDecimals}}`, token["com.twitter"], token["com.github"]], { gasLimit: 1000000, maxFeePerGas: gasPrice, maxPriorityFeePerGas: priorityFee })
+        // console.log(subdomainLabel, domainOwner, fullNode, token.contract)
+
+        // NOTE: integrate the token NAME into the subdomain configurator contract
+        // USE: token.data.name
+        console.log(subdomainLabel, domainOwner, fullNode)
+        let githubName = (token.data.links.repos_url && token.data.links.repos_url.github[0] && token.data.links.repos_url.github[0].split("/")[3]) ? token.data.links.repos_url.github[0].split("/")[3] : "";
+        let resolverInput = [token.data.name, token.data.links.homepage[0], `https://gateway.pinata.cloud/ipfs/${token.avatar.IpfsHash}`, `{\"rev\":0}`, token.data.links.twitter_screen_name, githubName]; // Github account should always be the fourth in the array
+        console.log("Resolver inputs", resolverInput);
+        let newSubdomainAddrTx = await subdomainConfigContract.configureNonEthSubdomain(subdomainLabel, domainOwner, fullNode, resolverInput, { gasLimit: 1000000, maxFeePerGas: gasPrice, maxPriorityFeePerGas: priorityFee })
 
         console.log("Waiting for", normalizedTicker, "configureSubdomain transaction to mine at https://etherscan.io/tx/" + newSubdomainAddrTx.hash)
+      } else {
+        console.log("Domain already configured, skipping registration")
       }
-    }
+    // }
   }
 }
 
