c46f78eff1ab0d14133c1636f749ca5b42358fea
shafu <shafu0x@gmail.com>
2023-04-12 09:42:12 +0200

    remove pausability
    - remove pausability
    - remove owner
    - save gas

--- contracts/TokenDao/TokenDao.sol
+++ contracts/TokenDao/TokenDao.sol
@@ -10,30 +10,13 @@ import "@openzeppelin/contracts@4.8.2/token/ERC20/extensions/ERC20Votes.sol";
 import "./TokenRescuer.sol";
 
 /// @custom:security-contact security@tkn.xyz
-contract TokenDao is ERC20, ERC20Burnable, Pausable, Ownable, ERC20Permit, ERC20Votes, TokenRescuer {
+contract TokenDao is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, TokenRescuer {
     constructor(address owner) ERC20("TokenDao", "TKN") ERC20Permit("TokenDao") {
-        // Transfer ownership immediately
-        _transferOwnership(owner);
-
         _mint(msg.sender, 1000000000 * 10 ** decimals());
-        // Contract is paused by default
-        // This has to be done after _mint, else minting will fail
-        _pause();
-    }
-
-    function pause() public view onlyOwner {
-        revert("TokenDao: TRANSFERS CAN NOT BE DISABLED, ONCE ACTIVATED");
-        // _pause();
-    }
-
-    function unpause() public onlyOwner {
-        require(paused(), "TokenDao: Token is not paused");
-        _unpause();
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
-        whenNotPaused
         override
     {
         super._beforeTokenTransfer(from, to, amount);
